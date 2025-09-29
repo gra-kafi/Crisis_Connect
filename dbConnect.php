c521756f34f06088465cf5d1e561d639bd0ea78a
Tanveer-2002 <md.tasnimislamtanveer@gmail.com>
2025-09-29 12:56:44 +0600

    Connect to database

---
+++ dbConnect.php
@@ -0,0 +1,16 @@
+<?php
+    define("hostName","localhost");
+    define("userName","root");
+    define("password","");
+    define("database","crisis_connect");
+    define("port","3306");
+
+    $connect = mysqli_connect(hostName,userName,password,database,port);
+
+    if(!$connect){
+        echo "connection Faild";
+    }
+    else{
+        echo "Connection Successful";
+    }
+?>
